6fbe3ac08c8e82f95a8200218d3bb86fdce774f4
James Wright <james@wrightomated.com>
2021-01-13 17:59:38 +1100

    cypress test

--- cypress/integration/shareTeam.spec.js
+++ cypress/integration/shareTeam.spec.js
@@ -21,9 +21,12 @@ context('Test that a team can be copied', () => {
         cy.get('.share-box:nth-child(1) .material-icons').click();
     });
 
-    // it('should load roster', () => {
-    //     cy.visit('/');
-
-    // })
+    it('should load roster', () => {
+        const roster = '{"teamId":3,"players":[{"playerName":"Erny","player":{"id":9,"position":"Beastman Runner Lineman","playerStats":[6,3,3,4,9],"cost":60,"skills":[30],"primary":["G","M","S"],"secondary":["A","P"]},"alterations":{"spp":0,"ni":0}}],"teamName":"The Horns","teamType":"Chaos Chosen","extra":{"dedicated_fans":1},"inducements":{},"treasury":1000}'
+        cy.visit('?code=t3t1000d1p9IThe%20Horns:Erny');
+        cy.get('.team-player-caption').should('have.text', 'Chaos Chosen Team Players');
+        expect(localStorage.getItem('roster')).to.eq(roster);
+    })
+    
 });
 
