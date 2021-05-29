d53e332ada4b612d900891874eb52eead2c2e294
James Wright <james@wrightomated.com>
2021-05-29 16:52:13 +1000

    store format

--- src/store/currentTeam.store.ts
+++ src/store/currentTeam.store.ts
@@ -3,9 +3,10 @@ import { teamData } from '../data/teams.data';
 import type { Writable } from 'svelte/store';
 import type { Team } from '../models/team.model';
 
-
 const currentTeamStore = () => {
-    const { subscribe, update, set }: Writable<Team> = writable<Team>(getTeam())
+    const { subscribe, update, set }: Writable<Team> = writable<Team>(
+        getTeam(),
+    );
     return {
         subscribe,
         update,
@@ -13,16 +14,15 @@ const currentTeamStore = () => {
         setCurrentTeamWithCode: (code: string) => {
             update((store) => {
                 return getTeamFromCode(code) || store;
-            })
+            });
         },
         setCurrentTeamWithId: (id: number) => {
             update((store) => {
                 return getTeamFromId(id) || store;
-            })
-        }
-    }
-}
-
+            });
+        },
+    };
+};
 
 const getTeamFromQuery = () => {
     const code = window.location.search.substring(1).split('=')[1];
@@ -38,10 +38,8 @@ const getTeamFromCode = (code: string) => {
 };
 
 const getTeamFromId = (id: number) => {
-    return teamData.teams.find(
-        (x) => x.id === id
-    );
-}
+    return teamData.teams.find((x) => x.id === id);
+};
 
 const getTeamFromStorage = () => {
     const team = localStorage.getItem('selectedTeam');
