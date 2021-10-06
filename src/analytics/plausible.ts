07b9d33e72c95d5679ef9c63fc522582fc6f144b
James Wright <james@wrightomated.com>
2021-10-06 20:28:39 +1100

    added prop to event

--- src/analytics/plausible.ts
+++ src/analytics/plausible.ts
@@ -1,17 +1,5 @@
 import type { TeamName } from '../models/team.model';
 
-// export const sendEventToPlausible = async (data: any) => {
-//     plausible();
-//     const url = 'https://plausible.io/api/event';
-//     await fetch(url, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(data),
-//     });
-// };
-
 export const sendNewTeamNameToPlausible = (teamName: TeamName) => {
-    (window as any).plausible('team-name', { teamName });
+    (window as any).plausible('team-name', { props: { teamName } });
 };
