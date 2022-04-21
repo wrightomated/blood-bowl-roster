0ded84e9d9f10dd4492e672e3e21d22cb3262c98
James Wright <james@wrightomated.com>
2022-04-22 07:09:40 +1000

    added missing files

---
+++ src/components/auth/loggedOut.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { currentUserStore } from '../../store/currentUser.store';
+    import { modalState } from '../../store/modal.store';
+    import Button from '../uiComponents/button.svelte';
+    import FootballSpinner from '../uiComponents/footballSpinner.svelte';
+</script>
+
+{#if $currentUserStore}
+    <FootballSpinner loadingText="Logging out" />
+{:else}
+    <div class="success">
+        <p>Successfully logged out!</p>
+        <Button clickFunction={() => modalState.close()}>Okay</Button>
+    </div>
+{/if}
+
+<style lang="scss">
+    .success {
+        text-align: center;
+        p {
+            font-family: var(--display-font);
+            font-size: 20px;
+        }
+    }
+</style>
---
+++ src/components/menu/userCircle.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { currentUserStore } from '../../store/currentUser.store';
+</script>
+
+{#if $currentUserStore}
+    <div class="initial">
+        {$currentUserStore.displayName?.charAt(0)?.toUpperCase()}
+    </div>
+{:else}
+    <i class="material-icons">account_circle</i>
+{/if}
+
+<style lang="scss">
+    .initial {
+        font-family: var(--display-font);
+        color: var(--main-colour);
+        background-color: white;
+        border-radius: 50%;
+        width: 28px;
+        height: 28px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid;
+
+        &:hover {
+            background-color: var(--main-colour);
+            color: white;
+        }
+    }
+    i {
+        font-size: 36px;
+    }
+</style>
