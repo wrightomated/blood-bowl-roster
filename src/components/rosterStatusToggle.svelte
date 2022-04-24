0dd095bc66133c4dbbd1a2b132869023626cde0a
James Wright <james@wrightomated.com>
2022-04-24 20:57:07 +1000

    status toggle

---
+++ src/components/rosterStatusToggle.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { LeagueRosterStatus } from '../models/roster.model';
+    import { roster } from '../store/teamRoster.store';
+    import ToggleButton from './uiComponents/toggleButton.svelte';
+    const leagueStatus: LeagueRosterStatus[] = ['draft', 'commenced'];
+</script>
+
+{#if $roster.leagueRosterStatus}
+    <ToggleButton
+        options={leagueStatus}
+        selectedIndex={leagueStatus.indexOf($roster.leagueRosterStatus)}
+        selected={(status) => {
+            roster.changeLeagueRosterStatus(status);
+        }}
+    />
+{/if}
