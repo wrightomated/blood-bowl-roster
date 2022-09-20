02609e950743ddc5233fd4e72c4a74ce3cdc0f1c
James Wright <james@wrightomated.com>
2022-09-21 06:49:15 +1000

    test data for unit test

---
+++ src/__tests__/data/specialRuleData.ts
@@ -0,0 +1,54 @@
+export const norseNoSpecialRuleString = 't31t950m0f0d1p144I';
+export const norseSpecialRuleString = 't31t950m0f0d1s3p144I';
+
+export const norseNoSpecialRuleRoster = {
+    teamId: 31,
+    players: [
+        {
+            playerName: '',
+            player: {
+                position: 'Norse Raider Lineman',
+                id: 144,
+                cost: 50,
+                playerStats: [6, 3, 3, 4, 8],
+                skills: [13, 93, 60],
+                primary: ['G'],
+                secondary: ['A', 'P', 'S'],
+            },
+            alterations: { spp: 0, ni: 0 },
+        },
+    ],
+    teamName: '',
+    teamType: 'Norse',
+    extra: { dedicated_fans: 1 },
+    inducements: {},
+    treasury: 950,
+    mode: 'league',
+    format: 'elevens',
+};
+
+export const norseSpecialRuleRoster = {
+    teamId: 31,
+    players: [
+        {
+            playerName: '',
+            player: {
+                position: 'Norse Raider Lineman',
+                id: 144,
+                cost: 50,
+                playerStats: [6, 3, 3, 4, 8],
+                skills: [13, 93, 60],
+                primary: ['G'],
+                secondary: ['A', 'P', 'S'],
+            },
+            alterations: { spp: 0, ni: 0 },
+        },
+    ],
+    teamName: '',
+    teamType: 'Norse',
+    extra: { dedicated_fans: 1, special_rule: 3 },
+    inducements: {},
+    treasury: 950,
+    mode: 'league',
+    format: 'elevens',
+};
