daff5f8bc85f5969d05a7c8e82898c82827beec8
James Wright <33916051+wrightomated@users.noreply.github.com>
2023-02-11 09:23:53 +1100

    Feature/individual totals (#121)
    * player individual events
    
    * minor style changes
    
    * fix: no game event bug

---
+++ src/components/gameHistory/playerEventList.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import { gameEventTypeToTitle } from '../../data/gameEventMap';
+    import type { GameEvent } from '../../models/matchHistory.model';
+    import { roster } from '../../store/teamRoster.store';
+    import MaterialButton from '../uiComponents/materialButton.svelte';
+
+    export let gameEvents: GameEvent[];
+    export let allowDeletion: boolean = false;
+
+    function removeEvent(gameEvent) {
+        $roster.matchDraft.playingCoach.gameEvents =
+            $roster.matchDraft.playingCoach.gameEvents.filter(
+                (e) => e !== gameEvent
+            );
+    }
+</script>
+
+<div class="player-event-grid">
+    {#each gameEvents.sort((a, b) => (a.turn || 0) - (b.turn || 0)) as gameEvent}
+        <div class="event">
+            <div class="player-number">
+                Player {gameEvent?.player?.number}
+            </div>
+            <div class="event-type">
+                {gameEventTypeToTitle[gameEvent.eventType]}
+                {gameEvent.turn ? `on turn ${gameEvent.turn}` : ''}
+            </div>
+            {#if allowDeletion}
+                <MaterialButton
+                    hoverText="Remove event"
+                    symbol="delete"
+                    clickFunction={() => removeEvent(gameEvent)}
+                />
+            {/if}
+        </div>
+    {/each}
+</div>
+
+<style lang="scss">
+    .player-event-grid {
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+        margin-top: 16px;
+
+        .event {
+            display: flex;
+            flex-wrap: wrap;
+            background-color: var(--secondary-background-colour);
+            border-radius: 8px;
+            padding: 8px;
+            align-items: center;
+            justify-content: space-between;
+            &-type {
+                text-align: left;
+            }
+        }
+
+        .player-number {
+            font-family: var(--display-font);
+        }
+    }
+</style>
