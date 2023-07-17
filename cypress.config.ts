5968193eabf4bd324c26484d58288edfa4ab5d92
James Wright <james@wrightomated.com>
2023-07-17 13:55:01 +1000

    remove cypress plugin reference

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  video: false,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+    video: false,
+    e2e: {
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        // setupNodeEvents(on, config) {
+        //   return require('./cypress/plugins/index.js')(on, config)
+        // },
+        baseUrl: 'https://bbroster.com/',
     },
-    baseUrl: 'https://bbroster.com/',
-  },
-})
+});
