2e91d332656e6c3522792f71e1009cc1c2df7654
James Wright <james@wrightomated.com>
2023-08-05 07:10:41 +1000

    version check fix

--- src/helpers/rosterVersionCheck.ts
+++ src/helpers/rosterVersionCheck.ts
@@ -3,7 +3,10 @@ export const CURRENT_ROSTER_VERSION = '3.0';
 export function versionCheck(roster: Roster): Roster {
     let updatedRoster = roster;
     if (!roster.version || roster?.version < CURRENT_ROSTER_VERSION) {
-        if (roster.format === 'dungeon bowl' && !roster.teamId.includes('db')) {
+        if (
+            roster.format === 'dungeon bowl' &&
+            !roster.teamId.toString().includes('db')
+        ) {
             updatedRoster = {
                 ...updatedRoster,
                 teamId: 'db' + updatedRoster.teamId,
