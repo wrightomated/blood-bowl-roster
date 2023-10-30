8b3f94fc005be4bdf1c398df8d7a82fbcf0e43fb
James Wright <james@wrightomated.com>
2023-10-30 20:45:23 +1100

    qr code pakage

---
+++ src/components/qrCode/qrCode.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { rosterToString } from '../../helpers/rosterToString';
+    import { roster } from '../../store/teamRoster.store';
+
+    const QRCode = require('qrcode');
+
+    const text = rosterToString($roster);
+
+    QRCode.toCanvas(
+        document.getElementById('qr-code-canvas'),
+        text,
+        function (error) {
+            if (error) console.error(error);
+            console.log('success!');
+        }
+    );
+</script>
+
+<canvas id="qr-code-canvas" />
