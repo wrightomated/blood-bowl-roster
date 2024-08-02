d24878e5b79f4a58fd10dcd6e3fda477465048a9
James Wright <james@wrightomated.com>
2024-08-03 08:10:41 +1000

    added back current advancmentlist

--- src/components/rmrr/rmrrPlayerAdvancement.svelte
+++ src/components/rmrr/rmrrPlayerAdvancement.svelte
@@ -17,12 +17,12 @@
     $: advancementCount = rosterPlayer?.alterations?.advancements || 0;
 </script>
 
-<RmrrCurrentAdvancement {rosterPlayer} {index} />
-
 {#if isRinger && advancementCount < 6}
+    <RmrrCurrentAdvancement {rosterPlayer} {index} />
     <RmrrSelectAdvancement {rosterPlayer} {index} />
 {/if}
 
 {#if !isRinger && ((skillStackingAllowed && advancementCount < 6) || !rosterPlayer.alterations?.advancements)}
+    <CurrentAdvancementList {rosterPlayer} {index} />
     <SelectAdvancement {rosterPlayer} {index} />
 {/if}
