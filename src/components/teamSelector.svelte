40c75efda7985663a172a20cbc8263d5c4d9f972
James Wright <33916051+wrightomated@users.noreply.github.com>
2024-12-24 15:39:56 +1100

    Feature/revert team selection (#204)
    * reverted team selection
    
    * fixed e2e

--- src/components/teamSelector.svelte
+++ src/components/teamSelector.svelte
@@ -23,6 +23,8 @@
     import type { RosterMode } from '../store/rosterMode.store';
     import { sendEventToAnalytics } from '../analytics/plausible';
     import Button from './uiComponents/button.svelte';
+    import { flip } from 'svelte/animate';
+    import { scale } from 'svelte/transition';
     import { showDungeonBowl } from '../store/showDungeonBowl.store';
     import type { TeamFormat } from '../types/teamFormat';
 
@@ -32,13 +34,13 @@
     import { customisationRules } from '../customisation/customisation.store';
     import { _ } from 'svelte-i18n';
     import LoadTeam from './loadTeam.svelte';
-    import TeamAutoComplete from './uiComponents/autocomplete/teamAutoComplete.svelte';
     import MaterialButton from './uiComponents/materialButton.svelte';
     import { modalState } from '../store/modal.store';
 
     export let teamList: (Team | CustomTeam)[];
 
     let includeNaf: boolean = true;
+    let includeRetired: boolean = false;
 
     const nafTeams = ['28', '29'];
     let rosterModes: RosterMode[] = ['league', 'exhibition'];
@@ -67,10 +69,14 @@
         }
     });
 
+    // let tiers: number[];
+    // $: tiers = [...new Set(teamList.map((x) => x.tier))].sort((a, b) => a - b);
+
     let sortedTeam: (Team | CustomTeam)[];
     $: sortedTeam = sortTeam()
         .filter((x) => $filteredTiers.includes(x.tier))
         .filter((x) => (!includeNaf ? !nafTeams.includes(x.id) : true))
+        .filter((x) => !x?.retired || (x.retired && includeRetired))
         .filter((x) =>
             searchTerm
                 ? $_('teams.names.' + x.id)
@@ -83,12 +89,6 @@
 
     const sortTeam = () => {
         return teamList.sort((a, b) => a.name.localeCompare(b.name));
-    };
-
-    const teamUpdated = (e) => {
-        if (e.detail.teamId) {
-            newTeam(e.detail.teamId);
-        }
     };
 
     const newTeam = (id: string) => {
@@ -125,6 +125,34 @@
         });
 
         document.body.scrollIntoView();
+    };
+
+    const tierToNumeral = (tier: number) => {
+        const lookup = {
+            X: 10,
+            IX: 9,
+            V: 5,
+            IV: 4,
+            I: 1,
+        };
+        let num = tier;
+        let roman = '';
+        let i;
+        for (i in lookup) {
+            while (num >= lookup[i]) {
+                roman += i;
+                num -= lookup[i];
+            }
+        }
+        return roman;
+    };
+
+    const toggleNaf = () => {
+        includeNaf = !includeNaf;
+    };
+
+    const toggleRetired = () => {
+        includeRetired = !includeRetired;
     };
 
     const toggleDungeonBowl = (show: boolean) => {
@@ -167,62 +195,108 @@
         {$customisationRules?.customContent?.createTeamTitle ||
             $_('creation.title')}
     </h2>
-    <div class="select-area">
-        {#if rosterModes.length > 1}
-            <div class="flex">
-                <ToggleButton
-                    options={rosterModes}
-                    selectedIndex={rosterModes.indexOf($rosterMode)}
-                    selected={(mode) => {
-                        rosterMode.set(mode);
-                    }}
-                />
-                <MaterialButton
-                    symbol="info"
-                    hoverText={'Information'}
-                    clickFunction={showModeInfo}
-                ></MaterialButton>
-            </div>
-        {/if}
-
-        {#if teamFormats.length > 1}
-            <div class="flex">
-                <ToggleButton
-                    options={teamFormats}
-                    selectedIndex={teamFormats.indexOf($teamFormat)}
-                    selected={(format) => {
-                        changeFormat(format);
-                    }}
-                />
-                <MaterialButton
-                    symbol="info"
-                    hoverText={'Information'}
-                    clickFunction={showFormatInfo}
-                ></MaterialButton>
-            </div>
-        {/if}
-
-        {#if $teamSelectionOpen}
-            <TeamAutoComplete
-                teamList={sortedTeam}
-                on:teamSelected={teamUpdated}
+    {#if rosterModes.length > 1}
+        <div class="flex">
+            <ToggleButton
+                options={rosterModes}
+                selectedIndex={rosterModes.indexOf($rosterMode)}
+                selected={(mode) => {
+                    rosterMode.set(mode);
+                }}
             />
+            <MaterialButton
+                symbol="info"
+                hoverText={'Information'}
+                clickFunction={showModeInfo}
+            ></MaterialButton>
+        </div>
+    {/if}
+
+    {#if teamFormats.length > 1}
+        <div class="flex">
+            <ToggleButton
+                options={teamFormats}
+                selectedIndex={teamFormats.indexOf($teamFormat)}
+                selected={(format) => {
+                    changeFormat(format);
+                }}
+            />
+            <MaterialButton
+                symbol="info"
+                hoverText={'Information'}
+                clickFunction={showFormatInfo}
+            ></MaterialButton>
+        </div>
+    {/if}
+
+    {#if $teamSelectionOpen}
+        <div class="button-container">
+            <div class="filter__tier">
+                Filter:
+                {#each $toggledTiers as _tier, i}
+                    <button
+                        on:click={() => toggledTiers.toggleTier(i + 1)}
+                        class:selected={$filteredTiers.includes(i + 1)}
+                        class="filter__button"
+                        title={$_('creation.tier', { values: { tier: i + 1 } })}
+                        >{tierToNumeral(i + 1)}</button
+                    >
+                {/each}
+                <button
+                    on:click={toggleNaf}
+                    title={$_('creation.naf')}
+                    class:selected={includeNaf}
+                    class="filter__button">N</button
+                >
+                <button
+                    on:click={toggleRetired}
+                    title={$_('creation.s')}
+                    class:selected={includeRetired}
+                    class="filter__button">S</button
+                >
+            </div>
+            <label class="filter__search">
+                {$_('common.search')}
+                <input
+                    class="search-input"
+                    type="search"
+                    bind:value={searchTerm}
+                    placeholder={$_('creation.type')}
+                />
+            </label>
+            <br />
+            <div>
+                {#each sortedTeam as team (team.id)}
+                    <button
+                        class="team-button rounded-button"
+                        animate:flip={{ duration: 200 }}
+                        transition:scale|local={{ duration: 200 }}
+                        class:selected={$currentTeam?.id === team?.id}
+                        on:click={() => newTeam(team.id)}
+                        >{$_('teams.names.' + team.id)}
+                        <span class="display-font"
+                            >{tierToNumeral(team.tier)}</span
+                        >{#if nafTeams.includes(team.id)}<span
+                                class="display-font">&nbsp;N</span
+                            >{/if}</button
+                    >
+                {/each}
+                {#if sortedTeam.length === 0}
+                    <p class="no-matches">No teams match your filter</p>
+                {/if}
+            </div>
             {#if $currentTeam?.pickSpecialRule}
                 <SelectSpecialRule />
             {/if}
+        </div>
 
-            {#if $currentTeam && !$currentTeamIsDungeonBowl}
-                <Button
-                    clickFunction={createTeam}
-                    cyData="create-team"
-                    disabled={!$currentTeam || $currentTeamIsDungeonBowl}
-                    >{$_('creation.create') +
-                        ' ' +
-                        $_('teams.names.' + $currentTeam?.id)} Team</Button
-                >
-            {/if}
-        {/if}
-    </div>
+        <Button
+            clickFunction={createTeam}
+            cyData="create-team"
+            disabled={!$currentTeam || $currentTeamIsDungeonBowl}
+            >{$_('creation.create')}</Button
+        >
+    {/if}
 {/if}
 
 {#if $teamLoadOpen}
@@ -238,8 +312,54 @@
         margin-block-end: 24px;
     }
 
-    .select-area {
-        max-width: 600px;
-        margin: 0 auto;
+    .button-container {
+        margin-top: 8px;
+        margin-bottom: 8px;
+        border-radius: 12px;
+        background: var(--secondary-background-colour);
+        padding: 8px;
+    }
+    .display-font {
+        font-family: var(--display-font);
+    }
+    .no-matches {
+        margin-left: 4px;
+    }
+
+    .filter {
+        &__tier {
+            display: inline-block;
+            margin: 1em 4px 1em 4px;
+        }
+        &__button {
+            font-family: var(--display-font);
+            border-radius: 50%;
+            font-size: 0.75em;
+            background-color: var(--secondary-compliment);
+            color: var(--secondary-colour);
+            padding: 0;
+            width: 24px;
+            height: 24px;
+            line-height: 0px;
+            text-align: center;
+            margin: 0 auto;
+            border: var(--secondary-border);
+            margin-right: 4px;
+
+            &:hover {
+                box-shadow: 0 4px 12px var(--button-shadow) inset;
+                background: var(--secondary-colour);
+                color: var(--secondary-compliment);
+            }
+            &.selected {
+                background-color: var(--secondary-colour);
+                color: var(--secondary-compliment);
+                border-color: var(--secondary-colour);
+            }
+        }
+        &__search {
+            display: inline-block;
+            margin: 0 4px 1rem 4px;
+        }
     }
 </style>
