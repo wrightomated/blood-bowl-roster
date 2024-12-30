7ed382c04dbe46c92b7d6cfdbc50cd4a2c96b4a3
James Wright <james@wrightomated.com>
2024-12-31 08:49:20 +1100

    add player card default translation

--- src/components/playerCard/addPlayerCard.svelte
+++ src/components/playerCard/addPlayerCard.svelte
@@ -130,7 +130,9 @@
                         disabled={playerTypeCounts[i]?.current >=
                             playerTypeCounts[i]?.max}
                     >
-                        {$_('players.' + playerType.id)}
+                        {$_('players.' + playerType.id, {
+                            default: playerType.position,
+                        })}
                         {playerTypeCounts[i]?.current}/{playerTypeCounts[i]
                             ?.max}
                     </option>
