2ddffd29207c2661ca816ee08a887ca842f4d4b8
James Wright <james@wrightomated.com>
2025-12-28 21:00:25 +1100

    remove banner

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,6 @@
     import Team from './components/team.svelte';
     import Modal from './components/uiComponents/modal.svelte';
     import Overlay from './components/uiComponents/overlay.svelte';
-    import Alert from './components/uiComponents/alert.svelte';
-    import { currentUserStore } from './store/currentUser.store';
     import type { TournamentCustomisation } from './customisation/types/TournmentCustomisation.type';
 
     const savedLocale = localStorage.getItem(localeStorageKey) || 'en';
@@ -66,33 +64,6 @@
     <Menu />
     <div>
         <main>
-            <Alert alertId="bb2025-transition" variant="info">
-                <p>
-                    <strong>BB Roster 2025 is coming!</strong> Read about the
-                    transition plan
-                    <a
-                        href="https://blog.bbroster.com/posts/blood-bowl-2025-transition"
-                        target="_blank"
-                        rel="noopener noreferrer">here</a
-                    >.
-                    {#if $currentUserStore}
-                        Your saved rosters will be available
-                        <a
-                            href="https://blog.bbroster.com/posts/saving-your-rosters"
-                            target="_blank"
-                            rel="noopener noreferrer">read here</a
-                        >.
-                    {:else}
-                        It looks like you're not logged in, read about keeping
-                        your rosters safe
-                        <a
-                            href="https://blog.bbroster.com/posts/saving-your-rosters"
-                            target="_blank"
-                            rel="noopener noreferrer">here</a
-                        >.
-                    {/if}
-                </p>
-            </Alert>
             <Team />
         </main>
     </div>
